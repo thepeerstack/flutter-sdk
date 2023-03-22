4f115481f903f89265f1e1ce1a39538bfb95f7bd
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2023-03-22 17:17:22 +0100

    build: upgrade flutter

--- example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/chiziaruhoma/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/chiziaruhoma/projects/packages/thepeer/example"
+export "FLUTTER_ROOT=/Users/idorenyin/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/idorenyin/Documents/GitHub/flutter-sdk/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "EXCLUDED_ARCHS=arm64"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
+export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
